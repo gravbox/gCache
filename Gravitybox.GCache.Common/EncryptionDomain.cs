b4b84df7e791b951ee1c470bf48f57f0cf5ca95a
Christopher Davis <chrisd@gravitybox.com>
2017-04-23 15:37:59 -0400

    added encryption class

---
+++ Gravitybox.GCache.Common/EncryptionDomain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gravitybox.GCache.Common
+{
+    public class EncryptionDomain
+    {
+        public byte[] Encrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            if (data == null)
+                return null;
+
+            var a = System.Security.Cryptography.Aes.Create();
+            a.Key = key;
+            a.IV = iv;
+            var e = a.CreateEncryptor(a.Key, a.IV);
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, e, CryptoStreamMode.Write))
+                {
+                    using (var sw = new StreamWriter(cs))
+                    {
+                        sw.Write(data);
+                    }
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            if (data == null)
+                return null;
+
+            var a = System.Security.Cryptography.Aes.Create();
+            a.Key = key;
+            a.IV = iv;
+            var e = a.CreateDecryptor(a.Key, a.IV);
+            using (var ms = new MemoryStream(data))
+            {
+                using (var cs = new CryptoStream(ms, e, CryptoStreamMode.Read))
+                {
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
